48b42f1f12e5d5a48c2a5f2b0c1d4ad94b162660
Jirilin <sjirilin@gmail.com>
2026-08-19 18:49:20 +0100

    Add main function for health and economic inequality analytics pipeline

---
+++ main.py
@@ -0,0 +1,46 @@
+from src.extract import extract_all
+from src.transform import transform_all
+from src.load_database import load_database
+from src.analyse import run_analysis
+
+def main():
+
+    print(
+        "\n"
+        "=========================================\n"
+        " HEIVA ENGLAND\n"
+        " Health & Economic Inequality Analytics\n"
+        "=========================================\n"
+    )
+
+    # 1. Extract
+    extract_all()
+
+    # 2. Transform
+    transform_all()
+
+    # 3. Load into SQL
+    load_database()
+
+    # 4. Analyse
+    run_analysis()
+
+    # Reload SQL after vulnerability score has been created.
+    load_database()
+
+    print(
+        "\n"
+        "=========================================\n"
+        " PROJECT PIPELINE COMPLETED\n"
+        "=========================================\n"
+    )
+
+    print(
+        "Next step:\n"
+        "Open data/processed/analytics_master.csv "
+        "in Power BI."
+    )
+
+
+if __name__ == "__main__":
+    main()
